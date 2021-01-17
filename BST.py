6d03460efe818018cfab49ff937ce05c6f268d1f
David Scullion <davidscullion82@gmail.com>
2021-01-17 11:10:06 +0000

    Added CLass Noce, Func insert and inorder

--- BST.py
+++ BST.py
@@ -1,3 +1,33 @@
+# Pyton impelementation of a BST with insert, search and inorder functions
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+# A utility function to insert a new node with a given key
+
+def insert(rootk, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root()
+        elif root.val < key:
+            root.right = insert(root.right, key)
+        elif root.val > key:
+            root.left = insert(root.right, key)
+    return root
+
+# A utility function to do inorder tree traversal
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
+
 # A utility function to search a given key in BST 
 def search(root,key): 
       
